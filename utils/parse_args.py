07a910db072cfc4f2f8f563a5150d9785a6c494b
Christopher Thai <cthai@omnislash.com>
2019-11-10 15:13:12 -0800

    Added more command line args

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -3,8 +3,10 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-lf', '--load_file', type=str, default='iris_data_for_cleansing.csv',
+    parser.add_argument('-lf', '--load_file', type=str, default='updated_iris.csv',
                         help='Specify file to load from input file')
+    parser.add_argument('-of', '--output_files', action='store_true',
+                        help='Specify output for individual parts in output folder')
 
     args = parser.parse_args()
 

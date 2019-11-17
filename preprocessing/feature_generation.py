de347e7d48da7ba8901c185d7234a10798e74ae2
Christopher Thai <cthai@omnislash.com>
2019-11-16 22:16:14 -0800

    More documentation on preprocessing

--- preprocessing/feature_generation.py
+++ preprocessing/feature_generation.py
@@ -3,35 +3,69 @@ import json
 
 
 def feature_generation_pca(df, features_to_generate):
+    """
+    Main method of feature_generation, runs code to generate PCA features off of the four original features
+
+    :param df: loaded data as a DataFrame
+    :param features_to_generate: Number of PCA features to return, default is 2
+    :return df: Loaded data as a DataFrame with PCA features added on
+    """
+    # Generate features
     generated_features, eigen_result = generate_features(df, features_to_generate)
 
+    # Add new features onto dataframe
     df = add_to_dataframe(df, generated_features)
 
+    # Output data to a csv
     output_feature_generation_result(df, eigen_result)
 
     return df
 
 
 def generate_features(df, features_to_generate, columns=None):
+    """
+    This method creates test statistics to pass to get_eigen_vectors
+
+    :param df: loaded data as a DataFrame
+    :param features_to_generate: Number of PCA features to return, default is 2
+    :param columns: Columns to generate PCA features on, default is original 4
+    :return (eigen_vectors.T.dot(distances)).T: Filtered eigen vectors times original data
+    :return eigen_result: Unfiltered eigen values and vectors as a dictionary
+    """
     if columns is None:
         columns = ["sepal length", "sepal width", "petal length", "petal width"]
 
+    # Calculate test statistics
     data_array = df[columns].values
     mean = np.mean(data_array, axis=0)
     covariance = np.cov(data_array.T)
+
+    # Retrieve eigen outcomes from get_eigen_vectors
     eigen_vectors, eigen_result = get_eigen_vectors(covariance, features_to_generate)
 
+    # Calculate distances dot product onto the filtered eigen vectors
     distances = (data_array - mean).T
 
     return (eigen_vectors.T.dot(distances)).T, eigen_result
 
 
 def get_eigen_vectors(covariance, features_to_generate):
+    """
+    Method takes covariance and generates eigen values and vectors using numpy.linalg.eig
+
+    :param covariance: Calculated covariance based on original data
+    :param features_to_generate: Number of eigen vectors to return, based on features_to_generate and eigen values
+    :return eigen_vectors[:, :features_to_generate]: Filters the top eigen vectors based on eigen values
+    :return eigen_result: Unfiltered eigen values and vectors as a dictionary
+    """
+    # Calculate eigen parameters
     eigen_values, eigen_vectors = np.linalg.eig(covariance)
 
+    # Sort eigen vectors by eigen values
     sort_eigen_values_index = eigen_values.argsort()[::-1]
     eigen_vectors = eigen_vectors[sort_eigen_values_index]
 
+    # Generate a dictionary using the original eigen parameters
     eigen_result = {}
     for index in range(len(eigen_values)):
         eigen_result.update({
@@ -42,9 +76,17 @@ def get_eigen_vectors(covariance, features_to_generate):
 
 
 def add_to_dataframe(df, generated_features):
+    """
+    Method to add new features on to original DataFrame
+
+    :param df: loaded data as a DataFrame
+    :param generated_features: Generated PCA features
+    :return df: Loaded data with the new PCA features
+    """
     df["PCA 1"] = generated_features[:, 0]
     df["PCA 2"] = generated_features[:, 1]
 
+    # Reorder the DataFrame so that class is the last column
     df = df[["sepal length", "sepal width", "petal length", "petal width", "New Feature 1", "New Feature 2", "PCA 1",
              "PCA 2", "class"]]
 
@@ -52,6 +94,12 @@ def add_to_dataframe(df, generated_features):
 
 
 def output_feature_generation_result(df, eigen_result):
+    """
+    Method for outputting current DataFrame and eigen parameters at the end of this module to a csv
+
+    :param df: Passed in DataFrame for this module
+    :param eigen_result: Unfiltered eigen values and vectors as a dictionary
+    """
     df.to_csv("output/part2_feature_generation_data.csv")
 
     with open("output/part2_feature_generation_algorithm.json", 'w') as file:
--- preprocessing/feature_preprocessing.py
+++ preprocessing/feature_preprocessing.py
@@ -2,24 +2,44 @@ import json
 
 
 def feature_preprocessing_confidence_interval(df):
+    """
+    Main method to preprocess data by removing outliers using z score normalization and confidence intervals
+
+    :param df: Loaded data as a DataFrame
+    :return df: df with removed outliers
+    """
     normalize_df = df.copy(deep=True)
 
+    # Normalize the data
     normalize_df = normalize_z_score(normalize_df)
 
+    # Construct confidence intervals and tag values outside of those intervals
     outlier_set, confidence_interval = construct_confidence_interval_and_find_outliers(normalize_df, 1.96)
 
+    # Remove values outside of those intervals
     df, outlier_df = remove_outliers(df, outlier_set)
 
+    # Output data to a csv
     output_feature_preprocessing_result(df, outlier_df, confidence_interval)
 
     return df
 
 
 def normalize_z_score(df):
+    """
+    Method to normalize all data based on their z scores. This method focuses on the four original features
+
+    :param df: Copy of loaded data as a DataFrame
+    :return df: Normalized df
+    """
+    # Z score normalization by column for the four original features
     for column in df.keys()[:-5]:
+
+        # Get column test statistics
         mean = df[column].mean()
         standard_deviation = df[column].std()
 
+        # Z score normalization
         for index in range(150):
             current_data = df.at[index, column]
             df.at[index, column] = (current_data - mean) / standard_deviation
@@ -28,20 +48,35 @@ def normalize_z_score(df):
 
 
 def construct_confidence_interval_and_find_outliers(df, t_value):
+    """
+    Method for constructing confidence intervals for each column (of the four original)
+
+    :param df: Normalized df
+    :param t_value: Test statistic for constructing confidence intervals
+    :return outlier_set: List of indices outside of confidence intervals
+    :return confidence_interval: Dictionary containing the calculated confidence intervals by column
+    """
+    # Use a set for the outlier set since only distinct indices matter
     outlier_set = set()
     confidence_interval = {}
 
+    # Calculate confidence intervals by column
     for column in df[["sepal length", "sepal width", "petal length", "petal width"]].keys():
+
+        # Calculate test statistic, break df into two parts
         mean = df[column].mean()
         slower = df.loc[df[column] < mean][column].mean()
         supper = df.loc[df[column] > mean][column].mean()
 
+        # Calculate upper and lower bounds
         lower_bound = slower - (t_value * (mean - slower))
         upper_bound = supper + (t_value * (supper - mean))
 
+        # Add to dictionary
         temp_dict = {column: {'lower_bound': lower_bound, 'upper_bound': upper_bound}}
         confidence_interval.update(temp_dict)
 
+        # Add any indices outside of the confidence interval to the outlier set
         for index in range(150):
             if df.at[index, column] < lower_bound:
                 outlier_set.add(index)
@@ -52,13 +87,31 @@ def construct_confidence_interval_and_find_outliers(df, t_value):
 
 
 def remove_outliers(df, outlier_set):
+    """
+    Method removes indices tagged as outliers from the DataFrame
+
+    :param df: Loaded data as a DataFrame, not normalized
+    :param outlier_set: Set with indices that were marked as outliers
+    :return df: DataFrame with outlier removes
+    :return removed_df: Dataframe with the outliers
+    """
+    # Create removed_df first with the outliers
     removed_df = df.loc[df.index.isin(outlier_set)]
+
+    # Set df equal to non outliers
     df = df.loc[~df.index.isin(outlier_set)]
 
     return df, removed_df
 
 
 def output_feature_preprocessing_result(df, outlier_df, confidence_interval):
+    """
+    Method for outputting current DataFrame and confidence intervals at the end of this module to a csv
+
+    :param df: Passed in DataFrame for this module
+    :param outlier_df: DataFrame with outliers
+    :param confidence_interval: Confidence Intervals by column
+    """
     df.to_csv("output/part3_feature_preprocessing_data.csv")
     outlier_df.to_csv("output/part3_feature_preprocessing_outlier_data.csv")
 
